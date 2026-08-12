e332676eed072669ab80b7b62c1cfc3c2c8770ea
D3m01iti0n-10v3rs <nguyenhungwhatever@gmail.com>
2026-08-12 15:53:17 +0700

    Delete stm32/servo directory

--- stm32/servo/servo.h
+++
@@ -1,9 +0,0 @@
-#ifndef SERVO_H
-#define SERVO_H
-
-#include "stm32f1xx_hal.h"
-
-void servo_init(TIM_HandleTypeDef* htim, uint32_t channel);
-void servo_write(uint32_t channel, uint8_t angle);
-
-#endif
